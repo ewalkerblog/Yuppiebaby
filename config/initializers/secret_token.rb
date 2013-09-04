7c8f05fc499ae06b02e1e821f789e4f669e53642
Erin Walker <ewalker@ewalkerblog.co.za>
2013-09-04 22:05:23 +0200

    Alles

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yuppiebaby::Application.config.secret_token = '23ac457039d98f40ccf4227eaa0664460c0b048294fc152ab8715f4ecc37c8f90a1899789481a93a39ac202e72708ae658b9d765691f5be0ed505b0da33d5cb3'
